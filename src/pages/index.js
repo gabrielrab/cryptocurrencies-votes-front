deed5b0218a1af713a7091d718de567735d9cebf
Gabriel Rabelo <contato.ideialisa@gmail.com>
2022-08-22 14:29:33 -0300

    chore: get calculates coins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "../components/CardList";
 import Layout from "../components/Layout";
+import apiService from "../services/api";
 
 export default function Index() {
+  const [votes, setVotes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await apiService.get(`/votes/calculate`);
+      setVotes(response.data);
+    })();
+  }, [votes]);
+
   return (
     <Layout>
       <h1>Vote in your favorite coin:</h1>
-      <CardList
-        list={[
-          {
-            code: "USDT",
-            name: "Tether",
-            votes: 3,
-          },
-          {
-            code: "BTC",
-            name: "Bitcoin",
-            votes: 1,
-          },
-          {
-            code: "KLV",
-            name: "Klever",
-            votes: 4,
-          },
-        ]}
-      />
+      <CardList list={votes} />
     </Layout>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://159.223.104.144",
+});
+
+export default api;
